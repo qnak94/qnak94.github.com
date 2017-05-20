a52255cc3641a5da0dd4e6ae77b328f07e25643f
Anton Kunakovskyj <qnak94@mail.ru>
2017-05-20 16:27:46 +0300

    full signs

--- script.js
+++ script.js
@@ -6,21 +6,40 @@ function start() {
 }
 function getHitLevel() {
     var val = document.getElementById('hit__val').value;
-    if(val<20)
+    if(val<85)
         return 'GUARDED';
-    else if(val<50)
+    else if(val<170)
         return 'WARNING';
     else
         return 'ALARM!';
 }
-//function getAccelLevel() {
-//}
+function getAccelLevel() {
+    var x_val = document.getElementById('X').value;
+    var y_val = document.getElementById('Y').value;
+    var z_val = document.getElementById('Z').value;
+
+    if(abs(x_val)<5)document.getElementById('X__res').value="GUARDED";
+    else if(abs(x_val) <10)document.getElementById('X__res').value="WARNING";
+    else document.getElementById('X__res').value="ALARM!";
+
+    if(abs(y_val)<5)document.getElementById('Y__res').value="GUARDED";
+    else if(abs(y_val) <10)document.getElementById('Y__res').value="WARNING";
+    else document.getElementById('Y__res').value="ALARM!";
+
+    if(abs(z_val)<5)document.getElementById('Z__res').value="GUARDED";
+    else if(abs(z_val) <10)document.getElementById('Z__res').value="WARNING";
+    else document.getElementById('Z__res').value="ALARM!";
+
+        function abs(accVal) {
+            if(accVal<0)
+                accVal=-accVal;
+            return accVal;
+        }
+}
 function getLockState() {
     var locks = {
         lf:document.getElementById('LF').checked,
         rf:document.getElementById('RF').checked,
-        lb:document.getElementById('LB').checked,
-        rb:document.getElementById('RB').checked,
         ca:document.getElementById('Ca').checked,
         bg:document.getElementById('Bg').checked
     }
@@ -30,17 +49,25 @@ function getLockState() {
     else document.getElementById('LF__res').value = "Open";
     if(locks.rf)document.getElementById('RF__res').value = "Closed";
     else document.getElementById('RF__res').value = "Open";
-    if(locks.lb)document.getElementById('LB__res').value = "Closed";
-    else document.getElementById('LB__res').value = "Open";
-    if(locks.rb)document.getElementById('RB__res').value = "Closed";
-    else document.getElementById('RB__res').value = "Open";
     if(locks.ca)document.getElementById('Ca__res').value = "Closed";
     else document.getElementById('Ca__res').value = "Open";
     if(locks.bg)document.getElementById('Bg__res').value = "Closed";
     else document.getElementById('Bg__res').value = "Open";
 }
+function getDistanceState() {
+    var dist = document.getElementById('distance').value;
+    if(dist<300)document.getElementById('signal__state').value = "Active";
+    else document.getElementById('signal__state').value = "Lost";
+}
+function getScopeState() {
+    if(document.getElementById('scope').checked)document.getElementById('scope__state').value = "Normal";
+    else document.getElementById('scope__state').value = "Crashed";
+}
 
 function writeRes() {
     document.getElementById('hit__res').value=getHitLevel();
     getLockState();
+    getAccelLevel();
+    getDistanceState();
+    getScopeState();
 }
